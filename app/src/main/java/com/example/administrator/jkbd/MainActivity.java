79fb24d7ab51c21b59fabbe77658e37c3aafa8d0
1947833215@qq.com <1947833215@qq.com>
2017-06-28 20:55:55 +0800

    修改主界面，添加随机测试方法

--- app/src/main/java/com/example/administrator/jkbd/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.jkbd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void test(View view) {
     }
 }
