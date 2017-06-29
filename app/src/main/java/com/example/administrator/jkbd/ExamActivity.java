b02b7cd51a8e6098d1bdf8473f824b72e07bd84a
1947833215@qq.com <1947833215@qq.com>
2017-06-29 14:23:15 +0800

    显示题目界面

---
+++ app/src/main/java/com/example/administrator/jkbd/ExamActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.jkbd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class ExamActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+
+    }
+}
