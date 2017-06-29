3d0b7762c7481e20dc3f8cf3f7532a65d209c69c
1947833215@qq.com <1947833215@qq.com>
2017-06-29 19:00:07 +0800

    添加ExamActivity类

--- app/src/main/java/com/example/administrator/jkbd/ExamActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/ExamActivity.java
@@ -1,7 +1,8 @@
 package com.example.administrator.jkbd;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 
 /**
  * Created by Administrator on 2017/6/29.
@@ -9,8 +10,8 @@ import android.view.Menu;
 
 public class ExamActivity extends AppCompatActivity {
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
-
+    public void onCreate(@Nullable Bundle savedInstanceState ) {
+         super.onCreate(savedInstanceState);
+        setContentView(R.layout.acyivity_exam);
     }
 }
