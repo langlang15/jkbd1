31cfc6765b788ba47b3c80ccd69a0ae2a0ec4425
1947833215@qq.com <1947833215@qq.com>
2017-06-30 23:32:06 +0800

    修改实体类

--- app/src/main/java/com/example/administrator/jkbd/ExamActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/ExamActivity.java
@@ -3,15 +3,38 @@ package com.example.administrator.jkbd;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.administrator.ExamApplication;
+import com.example.administrator.bean.ExamInfo;
 
 /**
  * Created by Administrator on 2017/6/29.
  */
 
 public class ExamActivity extends AppCompatActivity {
+    TextView tvExamInfo;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState ) {
          super.onCreate(savedInstanceState);
         setContentView(R.layout.acyivity_exam);
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        tvExamInfo = (TextView)findViewById(R.id.tv_examinfo);
+    }
+
+    private void initData() {
+        ExamInfo examInfo=ExamApplication.getInstance().getExamInfo();
+        if(examInfo!=null){
+            showData(examInfo);
+        }
+    }
+
+    private void showData(ExamInfo examInfo) {
+        tvExamInfo.setText(examInfo.toString());
+
     }
 }
--- app/src/main/java/com/example/administrator/jkbd/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/MainActivity.java
@@ -3,11 +3,7 @@ package com.example.administrator.jkbd;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-
-import com.example.administrator.bean.ExamInfo;
-import com.example.administrator.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,25 +12,16 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
+
+    }
+
+    private void initData() {
 
     }
 
     public void test(View view) {
-        OkHttpUtils<ExamInfo>utils=new OkHttpUtils<>(getApplicationContext());
-        String url="http://101.251.196.90:8080/JztkServer/examInfo";
-        utils.url(url).
-                targetClass(ExamInfo.class)
-                .execute(new OkHttpUtils.OnCompleteListener<ExamInfo>() {
-                    @Override
-                    public void onSuccess(ExamInfo result) {
-                        Log.e("main","result="+result);
-                    }
 
-                    @Override
-                    public void onError(String error) {
-                        Log.e("main","result="+error);
-                    }
-                });
         startActivity(new Intent(MainActivity.this,ExamActivity.class));
     }
 
