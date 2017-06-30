50cd9d6975930b3181ec9d1e7ff85c172bdee2bc
1947833215@qq.com <1947833215@qq.com>
2017-07-01 00:29:45 +0800

    显示考题

--- app/src/main/java/com/example/administrator/jkbd/ExamActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/ExamActivity.java
@@ -3,17 +3,22 @@ package com.example.administrator.jkbd;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.administrator.ExamApplication;
 import com.example.administrator.bean.ExamInfo;
+import com.example.administrator.bean.Question;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/6/29.
  */
 
 public class ExamActivity extends AppCompatActivity {
-    TextView tvExamInfo;
+    TextView tvExamInfo,tvExamTitle,tvOp1,tvOp2,tvOp3,tvOp4;
+    ImageView mImageView;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState ) {
          super.onCreate(savedInstanceState);
@@ -24,12 +29,33 @@ public class ExamActivity extends AppCompatActivity {
 
     private void initView() {
         tvExamInfo = (TextView)findViewById(R.id.tv_examinfo);
+        tvExamTitle = (TextView)findViewById(R.id.tv_exam_title);
+        tvOp1 = (TextView)findViewById(R.id.tv_op1);
+        tvOp2 = (TextView)findViewById(R.id.tv_op2);
+        tvOp3 = (TextView)findViewById(R.id.tv_op3);
+        tvOp4 = (TextView)findViewById(R.id.tv_op4);
+        mImageView= (ImageView) findViewById(R.id.im_exam_image);
     }
 
     private void initData() {
         ExamInfo examInfo=ExamApplication.getInstance().getExamInfo();
         if(examInfo!=null){
             showData(examInfo);
+        }
+        List<Question>examList=ExamApplication.getInstance().getmExamList();
+        if(examList!=null){
+            showQuestion(examList);
+        }
+    }
+
+    private void showQuestion(List<Question> examList) {
+        Question exam= examList.get(0);
+        if(exam!=null){
+            tvExamTitle.setText(exam.getQuestion());
+            tvOp1.setText(exam.getItem1());
+            tvOp2.setText(exam.getItem2());
+            tvOp3.setText(exam.getItem3());
+            tvOp4.setText(exam.getItem4());
         }
     }
 
