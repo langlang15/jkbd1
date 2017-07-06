17264aa6d8f0b185d750aa22da28f5c20a14203d
1947833215@qq.com <1947833215@qq.com>
2017-07-06 10:21:25 +0800

    错误答案显示红色，正确答案显示绿色

--- app/src/main/java/com/example/administrator/jkbd/ExamActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/ExamActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.Gallery;
@@ -31,51 +32,77 @@ import com.squareup.picasso.Picasso;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * Created by Administrator on 2017/6/29.
  */
 
 public class ExamActivity extends AppCompatActivity {
-    TextView tvExamInfo,tvExamTitle,tvOp1,tvOp2,tvOp3,tvOp4,tvload,tvNo,tv03,tv04,tvTime;
-    Gallery mGallery;
-    CheckBox cb01,cb02,cb03,cb04;
-    CheckBox[] cbs=new CheckBox[4];
-    LinearLayout layoutLoading,layout03,layout04;
+
+
+    TextView []tvOps=new TextView[4];
+    CheckBox[] cbs = new CheckBox[4];
+
     QuestionAdapter mAdapter;
-    ProgressBar dialog;
-    ImageView mImageView;
+
+
     IExamBiz biz;
-    boolean isLoadExamInfo=false;
-    boolean isLoadQuestions=false;
+    boolean isLoadExamInfo = false;
+    boolean isLoadQuestions = false;
     LoadQuestionBroadcast mLoadQuestionBroadcast;
     LoadExamBroadcast mLoadExamBroadcast;
 
-    boolean isLoadExamInfoRecevier=false;
-    boolean isLoadQuestionsRecevier=false;
-
+    boolean isLoadExamInfoRecevier = false;
+    boolean isLoadQuestionsRecevier = false;
+    @BindView(R.id.load_dialog)ProgressBar dialog;
+    @BindView(R.id.tv_load)TextView tvload;
+    @BindView(R.id.layout_loading)LinearLayout layoutLoading;
+    @BindView(R.id.tv_examinfo)TextView tvExamInfo;
+    @BindView(R.id.tv_time)TextView tvTime;
+    @BindView(R.id.tv_exam_no)TextView tvNo;
+    @BindView(R.id.tv_exam_title)TextView tvExamTitle;
+    @BindView(R.id.im_exam_image)ImageView mImageView;
+    @BindView(R.id.tv_op1)TextView tvOp1;
+    @BindView(R.id.tv_op2)TextView tvOp2;
+    @BindView(R.id.tv_03)TextView tv03;
+    @BindView(R.id.tv_op3)TextView tvOp3;
+    @BindView(R.id.layout_03)LinearLayout layout03;
+    @BindView(R.id.tv_04)TextView tv04;
+    @BindView(R.id.tv_op4)TextView tvOp4;
+    @BindView(R.id.layout_04)LinearLayout layout04;
+    @BindView(R.id.cb_01)CheckBox cb01;
+    @BindView(R.id.cb_02)CheckBox cb02;
+    @BindView(R.id.cb_03)CheckBox cb03;
+    @BindView(R.id.cb_04)CheckBox cb04;
+    @BindView(R.id.gallery)Gallery mGallery;
+    @BindView(R.id.btnnext)Button btnnext;
 
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState ) {
-         super.onCreate(savedInstanceState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.acyivity_exam);
-        mLoadExamBroadcast=new LoadExamBroadcast();
-        mLoadQuestionBroadcast=new LoadQuestionBroadcast();
+        ButterKnife.bind(this);
+        mLoadExamBroadcast = new LoadExamBroadcast();
+        mLoadQuestionBroadcast = new LoadQuestionBroadcast();
         setListener();
         initView();
         laodData();
-        biz=new ExamBiz();
+        biz = new ExamBiz();
 
     }
 
     private void setListener() {
-        registerReceiver(mLoadExamBroadcast,new IntentFilter(ExamApplication.LOAD_EXAM_INFO));
-        registerReceiver(mLoadQuestionBroadcast,new IntentFilter(ExamApplication.LOAD_EXAM_QUESTION));
+        registerReceiver(mLoadExamBroadcast, new IntentFilter(ExamApplication.LOAD_EXAM_INFO));
+        registerReceiver(mLoadQuestionBroadcast, new IntentFilter(ExamApplication.LOAD_EXAM_QUESTION));
     }
 
     private void laodData() {
         layoutLoading.setEnabled(false);//加载时不能点击
-        new Thread(new Runnable(){
+        new Thread(new Runnable() {
             @Override
             public void run() {
                 biz.beginExam();
@@ -84,85 +111,103 @@ public class ExamActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        dialog= (ProgressBar) findViewById(R.id.load_dialog);
-        layoutLoading= (LinearLayout) findViewById(R.id.layout_loading);
-        layout03= (LinearLayout) findViewById(R.id.layout_03);
-        layout04= (LinearLayout) findViewById(R.id.layout_04);
-        tvTime= (TextView) findViewById(R.id.tv_time);
-        tvExamInfo = (TextView)findViewById(R.id.tv_examinfo);
-        tvExamTitle = (TextView)findViewById(R.id.tv_exam_title);
-        tvOp1 = (TextView)findViewById(R.id.tv_op1);
-        tvOp2 = (TextView)findViewById(R.id.tv_op2);
-        tvOp3 = (TextView)findViewById(R.id.tv_op3);
-        tvOp4 = (TextView)findViewById(R.id.tv_op4);
-        mImageView= (ImageView) findViewById(R.id.im_exam_image);
-        tvload= (TextView) findViewById(R.id.tv_load);
-        tvNo= (TextView) findViewById(R.id.tv_exam_no);
+//        dialog = (ProgressBar) findViewById(R.id.load_dialog);
+//        layoutLoading = (LinearLayout) findViewById(R.id.layout_loading);
+//        layout03 = (LinearLayout) findViewById(R.id.layout_03);
+//        layout04 = (LinearLayout) findViewById(R.id.layout_04);
+//        tvTime = (TextView) findViewById(R.id.tv_time);
+//        tvExamInfo = (TextView) findViewById(R.id.tv_examinfo);
+//        tvExamTitle = (TextView) findViewById(R.id.tv_exam_title);
+//        tvOp1 = (TextView) findViewById(R.id.tv_op1);
+//        tvOp2 = (TextView) findViewById(R.id.tv_op2);
+//        tvOp3 = (TextView) findViewById(R.id.tv_op3);
+//        tvOp4 = (TextView) findViewById(R.id.tv_op4);
+//        mImageView = (ImageView) findViewById(R.id.im_exam_image);
+//        tvload = (TextView) findViewById(R.id.tv_load);
+//        tvNo = (TextView) findViewById(R.id.tv_exam_no);
+//
+//        cb01 = (CheckBox) findViewById(R.id.cb_01);
+//        cb02 = (CheckBox) findViewById(R.id.cb_02);
+//        cb03 = (CheckBox) findViewById(R.id.cb_03);
+//        cb04 = (CheckBox) findViewById(R.id.cb_04);
+//
+//        mGallery = (Gallery) findViewById(R.id.gallery);
+//        tv03 = (TextView) findViewById(R.id.tv_03);
+//        tv04 = (TextView) findViewById(R.id.tv_04);
 
-        cb01= (CheckBox) findViewById(R.id.cb_01);
-        cb02= (CheckBox) findViewById(R.id.cb_02);
-        cb03= (CheckBox) findViewById(R.id.cb_03);
-        cb04= (CheckBox) findViewById(R.id.cb_04);
 
-        mGallery= (Gallery) findViewById(R.id.gallery);
-        tv03= (TextView) findViewById(R.id.tv_03);
-        tv04= (TextView) findViewById(R.id.tv_04);
-        cbs[0]=cb01;
-        cbs[1]=cb02;
-        cbs[2]=cb03;
-        cbs[3]=cb04;
+        cbs[0] = cb01;
+        cbs[1] = cb02;
+        cbs[2] = cb03;
+        cbs[3] = cb04;
+        tvOps[0]=tvOp1;
+        tvOps[1]=tvOp2;
+        tvOps[2]=tvOp3;
+        tvOps[3]=tvOp4;
 
-        layoutLoading.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                laodData();
-                dialog.setVisibility(View.VISIBLE);//隐藏正在加载的图片
-                tvload.setText("下载数据...");
-            }
-        });
+//        layoutLoading.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                laodData();
+//                dialog.setVisibility(View.VISIBLE);//隐藏正在加载的图片
+//                tvload.setText("下载数据...");
+//            }
+//        });
         cb01.setOnCheckedChangeListener(listener);
         cb02.setOnCheckedChangeListener(listener);
         cb03.setOnCheckedChangeListener(listener);
         cb04.setOnCheckedChangeListener(listener);
     }
+    @OnClick(R.id.layout_loading)void onLoadClick(){
+        laodData();
+    }
 
-    CompoundButton.OnCheckedChangeListener listener=new CompoundButton.OnCheckedChangeListener (){
+    CompoundButton.OnCheckedChangeListener listener = new CompoundButton.OnCheckedChangeListener() {
 
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            if(isChecked){
-                int userAnswer=0;
-                switch (buttonView.getId()){
-                    case R.id.cb_01: userAnswer=1;break;
-                    case R.id.cb_02: userAnswer=2;break;
-                    case R.id.cb_03: userAnswer=3;break;
-                    case R.id.cb_04: userAnswer=4;break;
+            if (isChecked) {
+                int userAnswer = 0;
+                switch (buttonView.getId()) {
+                    case R.id.cb_01:
+                        userAnswer = 1;
+                        break;
+                    case R.id.cb_02:
+                        userAnswer = 2;
+                        break;
+                    case R.id.cb_03:
+                        userAnswer = 3;
+                        break;
+                    case R.id.cb_04:
+                        userAnswer = 4;
+                        break;
                 }
-                if(userAnswer>0){
-                    for(CheckBox cb:cbs){
+                if (userAnswer > 0) {
+                    for (CheckBox cb : cbs) {
                         cb.setChecked(false);
                     }
-                    cbs[userAnswer-1].setChecked(true);
+                    cbs[userAnswer - 1].setChecked(true);
                 }
             }
 
         }
     };
+
     private void initData() {
-        if(isLoadQuestionsRecevier&&isLoadExamInfoRecevier){
-            if(isLoadQuestions&&isLoadExamInfo){
+        if (isLoadQuestionsRecevier && isLoadExamInfoRecevier) {
+            if (isLoadQuestions && isLoadExamInfo) {
 
                 layoutLoading.setVisibility(View.GONE);
-                ExamInfo examInfo=ExamApplication.getInstance().getExamInfo();
-                if(examInfo!=null){
+                ExamInfo examInfo = ExamApplication.getInstance().getExamInfo();
+                if (examInfo != null) {
                     showData(examInfo);
                     initTimer(examInfo);
                 }
                 initGallery();
-                showQuestion( biz.getExam());
+                showQuestion(biz.getExam());
 
 
-            }else {
+            } else {
                 layoutLoading.setEnabled(true);
                 dialog.setVisibility(View.GONE);//隐藏正在加载的图片
                 tvload.setText("下载失败，点击重新");
@@ -173,7 +218,7 @@ public class ExamActivity extends AppCompatActivity {
     }
 
     private void initGallery() {
-        mAdapter=new QuestionAdapter(this);
+        mAdapter = new QuestionAdapter(this);
         mGallery.setAdapter(mAdapter);
         mGallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -185,25 +230,25 @@ public class ExamActivity extends AppCompatActivity {
     }
 
     private void initTimer(ExamInfo examInfo) {
-        int sumTime=examInfo.getLimitTime()*60*1000;
+        int sumTime = examInfo.getLimitTime() * 60 * 1000;
         //int sumTime=60*1000;
 
-        final long overTime= (sumTime+(System.currentTimeMillis()));
-        final Timer timer=new Timer();
+        final long overTime = (sumTime + (System.currentTimeMillis()));
+        final Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-               long l= overTime-System.currentTimeMillis();
-                final long min= (l/1000/60);
-                final long sec= (l/1000%60);
+                long l = overTime - System.currentTimeMillis();
+                final long min = (l / 1000 / 60);
+                final long sec = (l / 1000 % 60);
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        tvTime.setText("剩余时间："+min+"分"+sec+"秒");
+                        tvTime.setText("剩余时间：" + min + "分" + sec + "秒");
                     }
                 });
             }
-        },0,1000);//没1000毫秒刷新时间
+        }, 0, 1000);//没1000毫秒刷新时间
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
@@ -215,57 +260,92 @@ public class ExamActivity extends AppCompatActivity {
                     }
                 });
             }
-        },sumTime);
+        }, sumTime);
     }
 
 
     private void showQuestion(Question exam) {
-        Log.e("showQuestion","showQuestion,exam="+exam);
-        if(exam!=null){
+        Log.e("showQuestion", "showQuestion,exam=" + exam);
+        if (exam != null) {
             tvNo.setText(biz.getExamIndex());
             tvExamTitle.setText(exam.getQuestion());
             tvOp1.setText(exam.getItem1());
             tvOp2.setText(exam.getItem2());
             tvOp3.setText(exam.getItem3());
             tvOp4.setText(exam.getItem4());
-            layout03.setVisibility(exam.getItem3().equals("")?View.GONE:View.VISIBLE);
-            cb03.setVisibility(exam.getItem3().equals("")?View.GONE:View.VISIBLE);
-            layout04.setVisibility(exam.getItem3().equals("")?View.GONE:View.VISIBLE);
-            cb04.setVisibility(exam.getItem3().equals("")?View.GONE:View.VISIBLE);
-            if(exam.getUrl()!=null&&!exam.getUrl().equals(""))
-            {
+            layout03.setVisibility(exam.getItem3().equals("") ? View.GONE : View.VISIBLE);
+            cb03.setVisibility(exam.getItem3().equals("") ? View.GONE : View.VISIBLE);
+            layout04.setVisibility(exam.getItem3().equals("") ? View.GONE : View.VISIBLE);
+            cb04.setVisibility(exam.getItem3().equals("") ? View.GONE : View.VISIBLE);
+            if (exam.getUrl() != null && !exam.getUrl().equals("")) {
                 mImageView.setVisibility(View.VISIBLE);
                 Picasso.with(ExamActivity.this).load(exam.getUrl()).into(mImageView);
 
-            }else {
+            } else {
                 mImageView.setVisibility(View.GONE);
             }
             resetOption();
-           String userAnswer =exam.getUaserAnswer();
-            if(userAnswer!=null&& !userAnswer.equals("")){
-               int userCB= Integer.parseInt(userAnswer)-1;
+            String userAnswer = exam.getUaserAnswer();
+            if (userAnswer != null && !userAnswer.equals("")) {
+                int userCB = Integer.parseInt(userAnswer) - 1;
                 cbs[userCB].setChecked(true);
                 setOptions(true);
-            }else {
+                setAnswerTextColor(userAnswer,exam.getAnswer());
+            } else {
                 setOptions(false);
+                setOptionsColoe();
             }
         }
     }
-    private  void setOptions(boolean hasAnswer){
-        for(CheckBox cb:cbs){
+
+    private void setOptionsColoe() {
+        for(TextView tvOp:tvOps)
+        {
+            tvOp.setTextColor(getResources()
+                    .getColor(R.color.black));
+        }
+    }
+
+    private void setAnswerTextColor(String userAnswer, String answer) {
+        int ra=Integer.parseInt(answer)-1;
+        for(int i=0;i<tvOps.length;i++){
+            if(i==ra){
+                tvOps[i].setTextColor(getResources()
+                        .getColor(R.color.green));
+            }else {
+                if(!userAnswer.equals(answer)){
+                    int ua=Integer.parseInt(userAnswer)-1;
+                    if(i==ua){
+                        tvOps[i].setTextColor(getResources()
+                                .getColor(R.color.red));
+                    }else {
+                        tvOps[i].setTextColor(getResources()
+                                .getColor(R.color.black));
+                    }
+
+                }
+            }
+        }
+
+    }
+
+    private void setOptions(boolean hasAnswer) {
+        for (CheckBox cb : cbs) {
             cb.setEnabled(!hasAnswer);
         }
     }
+
     private void resetOption() {
-        for(CheckBox cb : cbs){
+        for (CheckBox cb : cbs) {
             cb.setChecked(false);
         }
     }
-    private  void saveUserAnswer(){
 
-        for(int i=0;i<cbs.length;i++){
-            if(cbs[i].isChecked()){
-                biz.getExam().setUaserAnswer(String.valueOf(i+1));
+    private void saveUserAnswer() {
+
+        for (int i = 0; i < cbs.length; i++) {
+            if (cbs[i].isChecked()) {
+                biz.getExam().setUaserAnswer(String.valueOf(i + 1));
                 mAdapter.notifyDataSetChanged();
                 return;
             }
@@ -277,10 +357,10 @@ public class ExamActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(mLoadExamBroadcast!=null){
+        if (mLoadExamBroadcast != null) {
             unregisterReceiver(mLoadExamBroadcast);
         }
-        if(mLoadQuestionBroadcast!=null){
+        if (mLoadQuestionBroadcast != null) {
             unregisterReceiver(mLoadQuestionBroadcast);
         }
     }
@@ -302,46 +382,47 @@ public class ExamActivity extends AppCompatActivity {
 
     public void commit(View view) {
         saveUserAnswer();
-        int s=biz.commitExam();
-        View inflate=View.inflate(this,R.layout.layout_result,null);
-        TextView tvResult= (TextView) inflate.findViewById(R.id.tv_result);
-        tvResult.setText("你的分数为\n"+s+"分");
-        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        int s = biz.commitExam();
+        View inflate = View.inflate(this, R.layout.layout_result, null);
+        TextView tvResult = (TextView) inflate.findViewById(R.id.tv_result);
+        tvResult.setText("你的分数为\n" + s + "分");
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setIcon(R.mipmap.exam_commit32x32).setTitle("交卷")
-         .setView(inflate)
+                .setView(inflate)
                 .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                finish();
-            }
-        });
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
         builder.create().show();
 
     }
 
-    class LoadExamBroadcast extends BroadcastReceiver{
+    class LoadExamBroadcast extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-           boolean isSuccess= intent.getBooleanExtra(ExamApplication.LOAD_DATA_SUCESS,false);
-            Log.e("LoadExamBroadcast","LoadExamBroadcast,isSuccss"+isSuccess);
-            if(isSuccess){
-                isLoadExamInfo=true;
+            boolean isSuccess = intent.getBooleanExtra(ExamApplication.LOAD_DATA_SUCESS, false);
+            Log.e("LoadExamBroadcast", "LoadExamBroadcast,isSuccss" + isSuccess);
+            if (isSuccess) {
+                isLoadExamInfo = true;
             }
-            isLoadExamInfoRecevier=true;
+            isLoadExamInfoRecevier = true;
             initData();
         }
     }
+
     class LoadQuestionBroadcast extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
             boolean isSuccess = intent.getBooleanExtra(ExamApplication.LOAD_DATA_SUCESS, false);
-            Log.e("LoadQuestionBroadcast","LoadQuestionBroadcast,isSuccss"+isSuccess);
+            Log.e("LoadQuestionBroadcast", "LoadQuestionBroadcast,isSuccss" + isSuccess);
             if (isSuccess) {
                 isLoadQuestions = true;
             }
-            isLoadQuestionsRecevier=true;
+            isLoadQuestionsRecevier = true;
             initData();
         }
     }
